5a40ae0cf8468314fadcd4242a224ebc4e3d7c75
Amaka Konwea <lawrahcollections@gmail.com>
2023-01-17 13:23:57 +0000

    Math app setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// eslint-disable-next-line
+class App extends React.Component {
+  render() {
+    return (<h1>Hello React</h1>
+    );
+  }
+}
+
+export default App;
