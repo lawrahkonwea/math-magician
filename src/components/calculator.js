f2c9042aeab1b4cb7b0ecbed5cb2635103dcc1a0
Amaka Konwea <lawrahcollections@gmail.com>
2023-01-20 08:34:51 +0000

    Refractor code with hooks

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,54 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from './logic/calculate';
 
-class CalcUserInterface extends React.Component { // eslint-disable-line
-  constructor(props) {
-    super(props);
-    this.state = { total: 0, next: null, operation: null };
-  }
-
-  handleClick = (event) => {
-    const buttonName = event.target.innerText;
-    const result = calculate(this.state, buttonName);
-    this.setState(result);
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <section>
-        <div className="container">
-          <div className="header">
-            <button type="button" className="tag">
-              {total}
-              {operation}
-              {next}
-            </button>
-          </div>
-          <div className="calc-button">
-            <button type="submit" onClick={this.handleClick}>AC</button>
-            <button type="submit" onClick={this.handleClick}>+/-</button>
-            <button type="submit" onClick={this.handleClick}>%</button>
-            <button type="submit" onClick={this.handleClick} className="box-color">÷</button>
-            <button type="submit" onClick={this.handleClick}>7</button>
-            <button type="submit" onClick={this.handleClick}>8</button>
-            <button type="submit" onClick={this.handleClick}>9</button>
-            <button type="submit" onClick={this.handleClick} className="box-color">x</button>
-            <button type="submit" onClick={this.handleClick}>4</button>
-            <button type="submit" onClick={this.handleClick}>5</button>
-            <button type="submit" onClick={this.handleClick}>6</button>
-            <button type="submit" onClick={this.handleClick} className="box-color">-</button>
-            <button type="submit" onClick={this.handleClick}>1</button>
-            <button type="submit" onClick={this.handleClick}>2</button>
-            <button type="submit" onClick={this.handleClick}>3</button>
-            <button type="submit" onClick={this.handleClick} className="box-color">+</button>
-            <button type="submit" onClick={this.handleClick} className="zero">0</button>
-            <button type="submit" onClick={this.handleClick}>.</button>
-            <button type="submit" onClick={this.handleClick} className="box-color">=</button>
-          </div>
-        </div>
-      </section>
-    );
-  }
+function handleClick(event, state, setter) {
+  const buttonName = event.target.innerText;
+  const result = calculate(state, buttonName);
+  setter(result);
 }
+
+const CalcUserInterface =() => { // eslint-disable-line
+  const [state, setter] = useState({ total: 0, next: null, operation: null });
+  const { total, next, operation } = state;
+  return (
+    <section>
+      <div className="container">
+        <div className="header">
+          <button type="button" className="tag">
+            {total}
+            {operation}
+            {next}
+          </button>
+        </div>
+        <div className="calc-button">
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>AC</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>+/-</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>%</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)} className="box-color">÷</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>7</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>8</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>9</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)} className="box-color">x</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>4</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>5</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>6</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)} className="box-color">-</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>1</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>2</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>3</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)} className="box-color">+</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)} className="zero">0</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)}>.</button>
+          <button type="submit" onClick={(event) => handleClick(event, state, setter)} className="box-color">=</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default CalcUserInterface;
