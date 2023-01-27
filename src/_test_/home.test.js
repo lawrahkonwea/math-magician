3722614865af09ea78742464bb1baa55a8573e52
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-01-27 14:21:03 +0100

    test home component

---
+++ src/_test_/home.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import HomePage from "../components/home";
+
+describe('test HomePage component', () => {
+  test('it renders correctly', () => {
+    const tree = render (<HomePage />)
+    expect (tree).toMatchSnapshot()
+  })
+});
