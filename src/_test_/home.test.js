69a61436e445fa6e4412872f24d5c12c8c7307db
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-01-27 14:33:05 +0100

    fix linter error

--- src/_test_/home.test.js
+++ src/_test_/home.test.js
@@ -1,11 +1,9 @@
-import { render } from "@testing-library/react";
-import HomePage from "../components/home";
+import { render } from '@testing-library/react';
+import HomePage from '../components/home';
 
 describe('test HomePage component', () => {
   test('it renders correctly', () => {
-    const tree = render (<HomePage />)
-    expect (tree).toMatchSnapshot()
-  })
+    const tree = render(<HomePage />);
+    expect(tree).toMatchSnapshot();
+  });
 });
-
-
