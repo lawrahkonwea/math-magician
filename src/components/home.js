6da8b9bd2623bbf862d6dae56949616727ee0ce8
Amaka Konwea <lawrahcollections@gmail.com>
2023-04-11 12:05:03 +0100

    Add glow to header and change lorem text

--- src/components/home.js
+++ src/components/home.js
@@ -5,20 +5,16 @@ const HomePage = () => (
     <h3>Welcome to our page</h3>
     <div className="home-content">
       <p>
-        Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Excepturi dicta earum,
-        omnis odioseba sed pariatur sapiente consequuntur
-        natus consectetur maxime error, aliquam, laborum
-        repellat magni reiciendis asperiores officia. Animi,
-        impedit?
+        Mathematics is the science of structure, order, and relation
+        that has evolved from elemental practices of counting, measuring,
+        and describing the shapes of objects. It deals with logical reasoning
+        and quantitative calculation, and its development has involved an increasing
+        degree of idealization and abstraction of its subject matter.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Excepturi dicta earum,
-        omnis odio sed pariatur sapiente consequuntur
-        natus consectetur maxime error, aliquam, laborum
-        repellat magni reiciendis asperiores officia. Animi,
-        impedit?
+        Since the 17th century, mathematics has been an indispensable
+        adjunct to the physical sciences and technology, and in more recent
+        times it has assumed a similar role in the quantitative aspects of the life sciences.
       </p>
 
     </div>
